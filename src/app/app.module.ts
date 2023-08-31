59e6978418e483daf6ca57bf5cdaa0ca4d2d9187
Amany Abdelfattah <Amany.Abdelfattah@finway-eg.com>
2023-08-31 11:16:32 +0300

    Calendar 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendaHeaderComponent } from './demo-utils/calenda-header/calenda-header.component';
+import { DemoModule } from './demo/DemoModule';
+import { DemoUtilsModule } from './demo-utils/DemoUtilsModule';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // CalendaHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    DemoModule,
+    DemoUtilsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
